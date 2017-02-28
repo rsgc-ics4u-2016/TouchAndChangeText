8f4753f223e15bd261c3e8e77421149a2d3ccccb
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-28 09:00:47 -0500

    Basic example with a single label.

--- TouchAndChangeText/GameScene.swift
+++ TouchAndChangeText/GameScene.swift
@@ -1,89 +1,100 @@
 //
 //  GameScene.swift
-//  TouchAndChangeText
+//  SimpleGame
 //
-//  Created by Russell Gordon on 2/28/17.
-//  Copyright © 2017 Russell Gordon. All rights reserved.
+//  Created by Russell Gordon on 11/27/16.
+//  Copyright © 2016 Russell Gordon. All rights reserved.
 //
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
+class GameScene : SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // MARK: Properties
+    let tileLabel = SKLabelNode(fontNamed: "Helvetica-Bold")
     
+    // This function runs once at the start of the game
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        backgroundColor = SKColor.white
+        
+        // Show the score
+        tileLabel.text = "1"
+        tileLabel.fontColor = SKColor.black
+        tileLabel.name = "tile"
+        tileLabel.fontSize = 96
+        tileLabel.zPosition = 150  // Make sure the label stays on top of everything else!
+        tileLabel.position = CGPoint(x: size.width / 2, y: size.height / 2) // Top-right corner
+        self.addChild(tileLabel)
+        
+    }
+    
+    
+    
+    // This allows the scene to respond to a touch
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        
+        // A touch actually has many points (since a finger is large) so
+        // only proceed if we can get the first touch
+        guard let touch = touches.first else {
+            return
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // Get the location of the first touch
+        let touchLocation = touch.location(in: self)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        // Now find all the obstacles currently colliding with Santa
+        if let tile = self.childNode(withName: "tile") as? SKLabelNode {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            // Check to see if the obstacle is intersecting with Santa
+            if tile.frame.contains(touchLocation) {
+                
+                // This obstacle is currently a "hit", so add it to the list of items colliding with Santa
+                if let text = tile.text {
+                    if text == "1" {
+                        tile.text = "0"
+                    } else {
+                        tile.text = "1"
+                    }
+                }
+                
+            }
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+            
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TouchAndChangeText/GameViewController.swift
+++ TouchAndChangeText/GameViewController.swift
@@ -1,55 +1,34 @@
 //
 //  GameViewController.swift
-//  TouchAndChangeText
+//  SimpleGame
 //
-//  Created by Russell Gordon on 2/28/17.
-//  Copyright © 2017 Russell Gordon. All rights reserved.
+//  Created by Russell Gordon on 11/27/16.
+//  Copyright © 2016 Russell Gordon. All rights reserved.
 //
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
-class GameViewController: UIViewController {
-
+class GameViewController : UIViewController {
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() // must call viewDidLoad() method on parent class (do not worry about what this means right now)
+        let scene = GameScene(size: CGSize(width: 2048, height: 1536)) // This will create our basic scene to make a game on. We need to give a size.
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        // Now we will configure the scene
+        let skView = self.view as! SKView   // Tell SpriteKit that the current view will be the one shown to user in app
+        skView.showsFPS = true              // Make sure a frames per second info text field will be shown
+        skView.showsNodeCount = true        // Keep track of how many nodes (visual elements) are on screen
+        // (The fewer nodes the better for performance reasons)
+        skView.ignoresSiblingOrder = true   // I will explain this option later.
+        scene.scaleMode = .aspectFill       // Scene will expand if necessary to fill device screen.
+        skView.presentScene(scene)          // Make the view show the scene we have configured
+        
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
+    
+    
     override var prefersStatusBarHidden: Bool {
-        return true
+        return true // this ensures the status bar on the device is hidden
     }
+    
 }
